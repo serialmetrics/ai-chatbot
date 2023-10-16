3ba2b91ce6b2f69efb4596792e6edd780d18556c
Amir <amir@serialmetrics.com>
2023-10-16 13:59:05 -0700

    aider: Remove GitHub link from header.
    # Aider chat conversation:
    
    USER: remote the github link since it now says `Upload Documents`
    
    ASSISTANT: Sure, I can help with that. The change will be made in the `components/header.tsx` file. Here is the edit block for the change:
    
    ```tsx
    components/header.tsx
    <<<<<<< HEAD
            <a
              target="_blank"
              href="https://github.com/vercel/nextjs-ai-chatbot/"
              rel="noopener noreferrer"
              className={cn(buttonVariants({ variant: 'outline' }))}
            >
              <IconGitHub />
              <span className="hidden ml-2 md:flex">GitHub</span>
            </a>
            <a
              href="https://github.com/vercel/nextjs-ai-chatbot/"
              target="_blank"
              className={cn(buttonVariants())}
            >
              <IconVercel className="mr-2" />
              <span className="hidden sm:block">Upload Documents</span>
              <span className="sm:hidden">Upload</span>
            </a>
    =======
            <a
              href="https://github.com/vercel/nextjs-ai-chatbot/"
              target="_blank"
              className={cn(buttonVariants())}
            >
              <IconVercel className="mr-2" />
              <span className="hidden sm:block">Upload Documents</span>
              <span className="sm:hidden">Upload</span>
            </a>
    >>>>>>> updated
    ```
    
    This change will remove the GitHub link from the header.

--- components/header.tsx
+++ components/header.tsx
@@ -54,15 +54,6 @@ export async function Header() {
       </div>
       <div className="flex items-center justify-end space-x-2">
         <a
-          target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: 'outline' }))}
-        >
-          <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
           href="https://github.com/vercel/nextjs-ai-chatbot/"
           target="_blank"
           className={cn(buttonVariants())}
