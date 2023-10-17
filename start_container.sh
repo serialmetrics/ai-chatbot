dfbfcf46b60634a31b288eddddee626eba81142e
Sergei Podushkin <nvrhood@gmail.com>
2023-10-17 11:23:58 +0400

    Updates to run container detached

--- start_container.sh
+++ start_container.sh
@@ -4,7 +4,7 @@ IMAGE_NAME=$(cat .env | grep -oP 'DOCKER_IMAGE_NAME = \K(\w+)')
 CONTR_NAME=$(cat .env | grep -oP 'DOCKER_CONTAINER_NAME = \K(\w+)')
 PORT=$(cat .env | grep -oP 'DOCKER_PORT = \K(\w+)')
 
-docker run -it --rm \
+docker run -it -d --rm \
     --name $CONTR_NAME \
     -p $PORT:$PORT \
     $IMAGE_NAME
