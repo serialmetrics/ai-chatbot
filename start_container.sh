ce266b98074a294c09c4d93e40cd2e1cd56c04ed
Sergei Podushkin <nvrhood@gmail.com>
2023-10-18 13:25:43 +0400

    update to fix .env file usage

--- start_container.sh
+++ start_container.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME=$(cat .env | grep -oP 'DOCKER_IMAGE_NAME = \K(\w+)')
 CONTR_NAME=$(cat .env | grep -oP 'DOCKER_CONTAINER_NAME = \K(\w+)')
 PORT=$(cat .env | grep -oP 'DOCKER_PORT = \K(\w+)')
-UPLOAD_DIR=$(cat .env.local | grep -oP 'STORE_PATH=\K(\w+)')
+UPLOAD_DIR=$(cat .env | grep -oP 'STORE_PATH=\K(\w+)')
 
 docker run -it -d --rm \
     --name $CONTR_NAME \
