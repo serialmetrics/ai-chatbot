683c7444fb091d091beed0754b0f50ad5feb95f8
Sergei Podushkin <nvrhood@gmail.com>
2023-10-18 12:56:06 +0400

    update for file upload support

--- start_container.sh
+++ start_container.sh
@@ -3,8 +3,10 @@
 IMAGE_NAME=$(cat .env | grep -oP 'DOCKER_IMAGE_NAME = \K(\w+)')
 CONTR_NAME=$(cat .env | grep -oP 'DOCKER_CONTAINER_NAME = \K(\w+)')
 PORT=$(cat .env | grep -oP 'DOCKER_PORT = \K(\w+)')
+UPLOAD_DIR=$(cat .env.local | grep -oP 'STORE_PATH=\K(\w+)')
 
 docker run -it -d --rm \
     --name $CONTR_NAME \
     -p $PORT:$PORT \
+    --mount src=`pwd`/${UPLOAD_DIR},target=/app/${UPLOAD_DIR},type=bind \
     $IMAGE_NAME
