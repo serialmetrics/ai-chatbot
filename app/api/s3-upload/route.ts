f6cc7066d932f7cd328d1c2f17fa965f4a45238e
Sergei Podushkin <nvrhood@gmail.com>
2024-04-13 06:17:22 +0400

    simplified s3 upload

--- app/api/s3-upload/route.ts
+++ app/api/s3-upload/route.ts
@@ -7,10 +7,10 @@ import { sanitizeKey, uuid } from "@/lib/s3-keys";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 
-type NextRouteHandler = (req: NextRequest, res: NextResponse) => Promise<void>;
+type NextRouteHandler = (req: NextRequest) => Promise<NextResponse>;
 
-type Configure = (options: Options) => Handler;
 type Handler = NextRouteHandler & { configure: Configure };
+type Configure = (options: Options) => Handler;
 
 type Options = S3Config & {
     key?: (req: NextRequest, filename: string) => string | Promise<string>;
@@ -22,126 +22,102 @@ let missingEnvs = (config: Record<string, any>): string[] => {
     return required.filter((key) => !config[key] || config.key === "");
 };
 
-let makeRouteHandler = (options: Options = {}): Handler => {
-    let route: NextRouteHandler = async function (req: NextRequest, res: NextResponse): Promise<void> {
-        let config = getConfig({
-            accessKeyId: options.accessKeyId,
-            secretAccessKey: options.secretAccessKey,
-            bucket: options.bucket,
-            region: options.region,
-            forcePathStyle: options.forcePathStyle,
-            endpoint: options.endpoint,
+export async function POST(req: NextRequest) {
+    let config = getConfig({});
+
+    const session = await auth()
+    if (!session) {
+        console.log('Not authenticated');
+        return NextResponse.json({}, { status: 401 });
+    }
+
+    let missing = missingEnvs(config);
+    if (missing.length > 0) {
+        console.log(`Next S3 Upload: Missing ENVs ${missing.join(", ")}`)
+        return NextResponse.json({
+            error: `Next S3 Upload: Missing ENVs ${missing.join(", ")}`
+        }, { status: 500 })
+    }
+    const req_json = await req.json()
+    let uploadType = req_json._nextS3?.strategy;
+    let filename = req_json.filename;
+
+    if (!filename) {
+        console.log("Filename is empty!")
+        return NextResponse.json({
+            error: "Filename is empty!"
+        }, { status: 500 });
+    }
+
+    let key = `next-s3-uploads/${sanitizeKey(filename)}`;
+    let { bucket, region, endpoint } = config;
+
+    if (uploadType === "presigned") {
+        let filetype = req_json.filetype;
+        let client = getClient(config);
+        let params = {
+            Bucket: bucket,
+            Key: key,
+            ContentType: filetype,
+            CacheControl: "max-age=630720000",
+        };
+
+        console.log('getSignedUrl params:', params)
+        const url = await getSignedUrl(
+            client,
+            new PutObjectCommand(params), {
+                expiresIn: 60 * 60,
+            }
+        );
+
+        return NextResponse.json({
+            key,
+            bucket,
+            region,
+            endpoint,
+            url,
+        }, { status: 200 });
+    } else {
+        console.log('uploadType:', uploadType);
+        let stsConfig: STSClientConfig = {
+            credentials: {
+                accessKeyId: config.accessKeyId,
+                secretAccessKey: config.secretAccessKey,
+            },
+            region,
+        };
+
+        let policy = {
+            Statement: [
+                {
+                    Sid: "Stmt1S3UploadAssets",
+                    Effect: "Allow",
+                    Action: ["s3:PutObject"],
+                    Resource: [`arn:aws:s3:::${bucket}/${key}`],
+                },
+            ],
+        };
+        console.log('Resource:', `arn:aws:s3:::${bucket}/${key}`);
+
+        let sts = new STSClient(stsConfig);
+
+        console.log('S3UploadWebToken:', policy);
+        let command = new GetFederationTokenCommand({
+            Name: "S3UploadWebToken",
+            Policy: JSON.stringify(policy),
+            DurationSeconds: 60 * 60, // 1 hour
         });
 
-        const session = await auth()
-        if (!session) {
-            console.log('Not authenticated');
-            res = NextResponse.json({}, { status: 401 });
-            return
-        }
+        let token = await sts.send(command);
+        console.log('Upload token:', token);
+        const return_data = {
+            token,
+            key,
+            bucket,
+            region,
+        };
+        console.log('return_data:', return_data);
 
-        let missing = missingEnvs(config);
-        if (missing.length > 0) {
-            console.log(`Next S3 Upload: Missing ENVs ${missing.join(", ")}`)
-            res = NextResponse.json({
-                error: `Next S3 Upload: Missing ENVs ${missing.join(", ")}`
-            }, { status: 500 })
-            return
-        }
-        const req_json = await req.json()
-        let uploadType = req_json._nextS3?.strategy;
-        let filename = req_json.filename;
-
-        if (!filename) {
-            console.log("Filename is empty!")
-            res = NextResponse.json({
-                error: "Filename is empty!"
-            }, { status: 500 });
-            return
-        }
-
-        let key = options.key
-            ? await Promise.resolve(options.key(req_json, filename))
-            : `next-s3-uploads/${uuid()}/${sanitizeKey(filename)}`;
-        let { bucket, region, endpoint } = config;
-
-        if (uploadType === "presigned") {
-            let filetype = req_json.filetype;
-            let client = getClient(config);
-            let params = {
-                Bucket: bucket,
-                Key: key,
-                ContentType: filetype,
-                CacheControl: "max-age=630720000",
-            };
-
-            console.log('getSignedUrl params:', params)
-            const url = await getSignedUrl(
-                client,
-                new PutObjectCommand(params), {
-                    expiresIn: 60 * 60,
-                }
-            );
-
-            res = NextResponse.json({
-                key,
-                bucket,
-                region,
-                endpoint,
-                url,
-            }, { status: 200 });
-            return;
-        } else {
-            console.log('uploadType:', uploadType);
-            let stsConfig: STSClientConfig = {
-                credentials: {
-                    accessKeyId: config.accessKeyId,
-                    secretAccessKey: config.secretAccessKey,
-                },
-                region,
-            };
-
-            let policy = {
-                Statement: [
-                    {
-                        Sid: "Stmt1S3UploadAssets",
-                        Effect: "Allow",
-                        Action: ["s3:PutObject"],
-                        Resource: [`arn:aws:s3:::${bucket}/${key}`],
-                    },
-                ],
-            };
-            console.log('Resource:', `arn:aws:s3:::${bucket}/${key}`);
-
-            let sts = new STSClient(stsConfig);
-
-            console.log('S3UploadWebToken:', policy);
-            let command = new GetFederationTokenCommand({
-                Name: "S3UploadWebToken",
-                Policy: JSON.stringify(policy),
-                DurationSeconds: 60 * 60, // 1 hour
-            });
-
-            let token = await sts.send(command);
-            console.log('Upload token:', token);
-            const return_data = {
-                token,
-                key,
-                bucket,
-                region,
-            };
-            console.log('return_data:', return_data);
-
-            res = NextResponse.json(return_data, { status: 200 });
-            return;
-        }
-    };
-
-    let configure = (options: Options) => makeRouteHandler(options);
-
-    return Object.assign(route, { configure });
+        return NextResponse.json(return_data, { status: 200 });
+    }
 };
-
-let APIRoute = makeRouteHandler();
-
-export { APIRoute as POST, APIRoute as GET };
